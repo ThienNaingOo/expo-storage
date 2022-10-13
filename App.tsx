97f1b1cd52bd86ecef7757e4329a1ff56447c3db
TheinNaingOo <scm.theinnaingoo@gmail.com>
2022-10-13 14:41:39 +0630

    project_setup

--- App.tsx
+++ App.tsx
@@ -1,9 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { AsyncStorage } from 'react-native';
 
 export default function App() {
+  const [value, setValue] = useState("")
+  const storeFunction = () => {
+    const key0 = 32847239472
+    AsyncStorage.setItem("ITEM", JSON.stringify(key0))
+  }
+
+  const getFunction = () => {
+    AsyncStorage.getItem("ITEM").then((value) => {
+      console.log(typeof value);
+      setValue(value)
+    })
+  }
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      })
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{value}</Text>
+      <TouchableHighlight onPress={() => storeFunction()}><Text>set item</Text></TouchableHighlight>
+      <TouchableHighlight onPress={() => getFunction()}><Text>get item</Text></TouchableHighlight>
     </View>
   );
 }
